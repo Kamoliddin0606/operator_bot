ad030de680fd8a384fc719e864e19ab309322050
Kamoliddin0606 <3368606@gmail.com>
2023-08-27 16:52:28 +0500

    end

--- handlers/groups/myID.py
+++ handlers/groups/myID.py
@@ -11,3 +11,9 @@ async def getMyID(message: types.Message):
 @dp.message_handler( commands='myid')
 async def getMyID(message: types.Message):
     await message.reply("<b>🆔 Ваш идентификационный номер:</b> "+ "<i>"+str(message.from_user.id)+"</i>")  
+@dp.message_handler( text="🆔 группы и темы")
+async def getMyID(message: types.Message):
+     if message.chat.type == 'group' or message.chat.type == 'supergroup':
+        topic_id = message.message_thread_id
+       # await message.reply(f"The topic ID is: {topic_id}")
+        await message.reply("<i>🆔 номер группы: "+str(message.chat.id)+"\n🆔 номер темы: " +str(topic_id)+"</i>") 
